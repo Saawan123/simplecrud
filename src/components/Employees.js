f48f3df11249d7e1315f6a31316ea20ecc13b2c2
Saawan Kumar <saawankumar@Saawan-Mac-mini.local>
2023-01-05 17:51:56 +0530

    crud operations

---
+++ src/components/Employees.js
@@ -0,0 +1,19 @@
+const Employees =[
+    {
+        id:"1",
+        Name:"Saawan Kumar",
+        Age:"20"
+    },
+    {
+        id:"2",
+        Name:"Pratyush",
+        Age:"30"
+    },
+    {
+        id:"3",
+        Name:"Kamender",
+        Age:"40"
+    }
+]
+
+export default Employees;
